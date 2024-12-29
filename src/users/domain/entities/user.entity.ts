02a0bf477eb055e05d18663ed04939bcbec8419d
Jorge Campos de Jesus <jorgecamposdevs@gmail.com>
2024-12-29 17:56:56 -0300

    added user entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type Userprops = {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+};
+
+export class UserEntity {
+  constructor(public readonly props: Userprops) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+}
