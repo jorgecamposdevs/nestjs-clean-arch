cb73f187fbb995015a8aea5a05af3dea2765143a
Jorge Campos de Jesus <jorgecamposdevs@gmail.com>
2024-12-29 16:12:02 -0300

    refactoring env config module

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,23 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { EnvConfigService } from './env-config.service';
+import { join } from 'path';
 
-@Module({
-  providers: [EnvConfigService]
-})
-export class EnvConfigModule {}
+@Module({})
+export class EnvConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return {
+      module: EnvConfigModule,
+      imports: [
+        ConfigModule.forRoot({
+          ...options,
+          envFilePath: [
+            join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+          ],
+        }),
+      ],
+      providers: [EnvConfigService],
+      exports: [EnvConfigService],
+    };
+  }
+}
